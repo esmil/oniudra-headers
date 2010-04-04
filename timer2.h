6af8129c80c863cd0bbc9ebd315959e648baab3f
Emil Renner Berthing <esmil@mailme.dk>
2010-04-04 22:15:00 +0200

    timer2: clean up and add more functions and macros

--- timer2.h
+++ timer2.h
@@ -1,5 +1,5 @@
-#ifndef _ARDUINO_TIMERS_H
-#define _ARDUINO_TIMERS_H
+#ifndef _ARDUINO_TIMER2_H
+#define _ARDUINO_TIMER2_H
 
 #include <stdint.h>
 #include <avr/io.h>
@@ -17,27 +17,126 @@ timer2_mode_fastpwm()       { TCCR2A = _BV(WGM21) | _BV(WGM20); }
 
 /* timer2 clocks select */
 static inline void
-timer2_clock_off()   { TCCR2B = 0; }
+timer2_clock_off()
+{
+	TCCR2B = TCCR2B & ~(_BV(CS22) | _BV(CS21) | _BV(CS20));
+}
+
 static inline void
-timer2_clock_d1()    { TCCR2B = _BV(CS20); }
+timer2_clock_d1()
+{
+	TCCR2B = (TCCR2B & ~(_BV(CS22) | _BV(CS21) | _BV(CS20)))
+	       | _BV(CS20);
+}
+
 static inline void
-timer2_clock_d8()    { TCCR2B = _BV(CS21); }
+timer2_clock_d8()
+{
+	TCCR2B = (TCCR2B & ~(_BV(CS22) | _BV(CS21) | _BV(CS20)))
+	       | _BV(CS21);
+}
+
 static inline void
-timer2_clock_d32()   { TCCR2B = _BV(CS21) | _BV(CS20); }
+timer2_clock_d32()
+{
+	TCCR2B = (TCCR2B & ~(_BV(CS22) | _BV(CS21) | _BV(CS20)))
+	       | _BV(CS21) | _BV(CS20);
+}
+
 static inline void
-timer2_clock_d64()   { TCCR2B = _BV(CS22); }
+timer2_clock_d64()
+{
+	TCCR2B = (TCCR2B & ~(_BV(CS22) | _BV(CS21) | _BV(CS20)))
+	       | _BV(CS22);
+}
+
 static inline void
-timer2_clock_d128()  { TCCR2B = _BV(CS22) | _BV(CS20); }
+timer2_clock_d128()
+{
+	TCCR2B = (TCCR2B & ~(_BV(CS22) | _BV(CS21) | _BV(CS20)))
+	       | _BV(CS22) | _BV(CS20);
+}
+
 static inline void
-timer2_clock_d256()  { TCCR2B = _BV(CS22) | _BV(CS21); }
+timer2_clock_d256()
+{
+	TCCR2B = (TCCR2B & ~(_BV(CS22) | _BV(CS21) | _BV(CS20)))
+	       | _BV(CS22) | _BV(CS21);
+}
+
 static inline void
-timer2_clock_d1024() { TCCR2B = _BV(CS22) | _BV(CS21) | _BV(CS20); }
+timer2_clock_d1024()
+{
+	TCCR2B = (TCCR2B & ~(_BV(CS22) | _BV(CS21) | _BV(CS20)))
+	       | _BV(CS22) | _BV(CS21) | _BV(CS20);
+}
 
 static inline uint8_t
-timer2_clock_running() { return TCCR2B & (_BV(CS22) | _BV(CS21) | _BV(CS20)); }
+timer2_clock_running()
+{
+	return TCCR2B & (_BV(CS22) | _BV(CS21) | _BV(CS20));
+}
 
 static inline void
-timer2_clock_reset() { GTCCR = _BV(PSRASY); }
+timer2_clock_reset()
+{
+	GTCCR |= _BV(PSRASY);
+}
+
+/* timer2 pins */
+static inline void
+timer2_pin11_off()
+{
+	TCCR2A = TCCR2A & ~(_BV(COM2A1) | _BV(COM2A0));
+}
+
+static inline void
+timer2_pin11_toggle()
+{
+	TCCR2A = (TCCR2A & ~(_BV(COM2A1) | _BV(COM2A0)))
+	       | _BV(COM2A0);
+}
+
+static inline void
+timer2_pin11_clear()
+{
+	TCCR2A = (TCCR2A & ~(_BV(COM2A1) | _BV(COM2A0)))
+	       | _BV(COM2A1);
+}
+
+static inline void
+timer2_pin11_set()
+{
+	TCCR2A = (TCCR2A & ~(_BV(COM2A1) | _BV(COM2A0)))
+	       | _BV(COM2A1) | _BV(COM2A0);
+}
+
+static inline void
+timer2_pin3_off()
+{
+	TCCR2A = TCCR2A & ~(_BV(COM2B1) | _BV(COM2B0));
+}
+
+static inline void
+timer2_pin3_toggle()
+{
+	TCCR2A = (TCCR2A & ~(_BV(COM2B1) | _BV(COM2B0)))
+	       | _BV(COM2B0);
+}
+
+static inline void
+timer2_pin3_clear()
+{
+	TCCR2A = (TCCR2A & ~(_BV(COM2B1) | _BV(COM2B0)))
+	       | _BV(COM2B1);
+}
+
+static inline void
+timer2_pin3_set()
+{
+	TCCR2A = (TCCR2A & ~(_BV(COM2B1) | _BV(COM2B0)))
+	       | _BV(COM2B1) | _BV(COM2B0);
+}
 
 /* timer2 interrupts */
 static inline void
@@ -56,21 +155,37 @@ timer2_interrupt_b_disable()   { TIMSK2 &= ~(_BV(OCIE2B)); }
 #define timer2_interrupt_ovf() ISR(TIMER2_OVF_vect)
 #define timer2_interrupt_ovf_naked() ISR(TIMER2_OVF_vect, ISR_NAKED)
 #define timer2_interrupt_ovf_empty() EMPTY_INTERRUPT(TIMER2_OVF_vect)
-
-/* timer2 flags */
-static inline void
-timer2_flags_clear()   { TIFR2 = 0; }
-static inline uint8_t
-timer2_flag_overflow() { return TIFR2 & _BV(TOV2); }
-static inline uint8_t
-timer2_flag_a()        { return TIFR2 & _BV(OCF2A); }
-static inline uint8_t
-timer2_flag_b()        { return TIFR2 & _BV(OCF2B); }
+#define timer2_interrupt_a() ISR(TIMER2_COMPA_vect)
+#define timer2_interrupt_a_naked() ISR(TIMER2_COMPA_vect, ISR_NAKED)
+#define timer2_interrupt_a_empty() EMPTY_INTERRUPT(TIMER2_COMPA_vect)
+#define timer2_interrupt_b() ISR(TIMER2_COMPB_vect)
+#define timer2_interrupt_b_naked() ISR(TIMER2_COMPB_vect, ISR_NAKED)
+#define timer2_interrupt_b_empty() EMPTY_INTERRUPT(TIMER2_COMPB_vect)
 
 /* timer2 count */
 static inline uint8_t
 timer2_count()              { return TCNT2; }
 static inline void
 timer2_count_set(uint8_t n) { TCNT2 = n; }
+
+/* timer2 compare registers */
+static inline uint8_t
+timer2_compare_a()              { return OCR2A; }
+static inline uint8_t
+timer2_compare_b()              { return OCR2B; }
+static inline void
+timer2_compare_a_set(uint8_t v) { OCR2A = v; }
+static inline void
+timer2_compare_b_set(uint8_t v) { OCR2B = v; }
+
+/* timer2 flags */
+static inline void
+timer2_flags_clear() { TIFR2 = 0; }
+static inline uint8_t
+timer2_flag_ovf()    { return TIFR2 & _BV(TOV2); }
+static inline uint8_t
+timer2_flag_a()      { return TIFR2 & _BV(OCF2A); }
+static inline uint8_t
+timer2_flag_b()      { return TIFR2 & _BV(OCF2B); }
 
 #endif
