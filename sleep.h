8d514d21009e3dbbb509f3fd6a14cd557d54c73d
Emil Renner Berthing <esmil@mailme.dk>
2010-09-23 22:16:50 +0200

    sleep: set sleep modes properly
    ..that is without changing the SE bit

--- sleep.h
+++ sleep.h
@@ -23,16 +23,39 @@
 
 /* set sleep mode */
 static inline void
-sleep_mode_idle()             { SMCR = 0; }
+sleep_mode_idle()
+{
+	SMCR = SMCR & ~(_BV(SM2) | _BV(SM1) | _BV(SM0));
+}
+
 static inline void
-sleep_mode_noise_reduction()  { SMCR = _BV(SM0); }
+sleep_mode_noise_reduction()
+{
+	SMCR = (SMCR & ~(_BV(SM2) | _BV(SM1))) | _BV(SM0);
+}
+
 static inline void
-sleep_mode_power_down()       { SMCR = _BV(SM1); }
+sleep_mode_power_down()
+{
+	SMCR = (SMCR & ~(_BV(SM2) | _BV(SM0))) | _BV(SM1);
+}
+
 static inline void
-sleep_mode_power_save()       { SMCR = _BV(SM1) | _BV(SM0); }
+sleep_mode_power_save()
+{
+	SMCR = (SMCR & ~(_BV(SM2))) | _BV(SM1) | _BV(SM0);
+}
+
 static inline void
-sleep_mode_standby()          { SMCR = _BV(SM2) | _BV(SM1); }
+sleep_mode_standby()
+{
+	SMCR = (SMCR & ~(_BV(SM0))) | _BV(SM2) | _BV(SM1);
+}
+
 static inline void
-sleep_mode_external_standby() { SMCR = _BV(SM2) | _BV(SM1) | _BV(SM0); }
+sleep_mode_external_standby()
+{
+	SMCR = SMCR | _BV(SM2) | _BV(SM1) | _BV(SM0);
+}
 
 #endif
