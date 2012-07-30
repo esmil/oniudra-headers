8c9290d53b9ac72d878fd3adf1804cc693713972
Emil Renner Berthing <esmil@mailme.dk>
2012-07-30 18:03:18 +0200

    ATmegaX8/spi: rewrite

--- ATmegaX8/spi.h
+++ ATmegaX8/spi.h
@@ -22,56 +22,51 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define SS   10
+#define MOSI 11
+#define MISO 12
+#define SCLK 13
+
+enum spi_intstatus {
+	SPI_INTERRUPT_DISABLED = 0,
+	SPI_INTERRUPT_ENABLED  = _BV(SPIE),
+};
+
+enum spi_role {
+	SPI_SLAVE  = 0,
+	SPI_MASTER = _BV(MSTR),
+};
+
+enum spi_endianess {
+	SPI_MSB = 0,
+	SPI_LSB = _BV(DORD),
+};
+
+enum spi_mode {
+	SPI_MODE0 = 0,
+	SPI_MODE1 = _BV(CPHA),
+	SPI_MODE2 = _BV(CPOL),
+	SPI_MODE3 = _BV(CPOL) | _BV(CPHA),
+};
+
+enum spi_clock {
+	SPI_CLOCK_D4   = 0,
+	SPI_CLOCK_D16  = _BV(SPR0),
+	SPI_CLOCK_D64  = _BV(SPR1),
+	SPI_CLOCK_D128 = _BV(SPR1) | _BV(SPR0),
+};
+
+static inline void
+spi_off(void)         { SPCR = 0; }
+static inline void
+spi_config(uint8_t v) { SPCR = _BV(SPE) | v; }
+
 static inline void
 spi_interrupt_enable(void)    { SPCR |= _BV(SPIE); }
 static inline void
 spi_interrupt_disable(void)   { SPCR &= ~(_BV(SPIE)); }
 static inline uint8_t
 spi_interrupt_enabled(void)   { return SPCR & _BV(SPIE); }
-static inline void
-spi_enable(void)              { SPCR |= _BV(SPE); }
-static inline void
-spi_disable(void)             { SPCR &= ~(_BV(SPE)); }
-static inline void
-spi_data_order_lsb(void)      { SPCR |= _BV(DORD); }
-static inline void
-spi_data_order_msb(void)      { SPCR &= ~(_BV(DORD)); }
-static inline void
-spi_mode_master(void)         { SPCR |= _BV(MSTR); }
-static inline void
-spi_mode_slave(void)          { SPCR &= ~(_BV(MSTR)); }
-static inline void
-spi_clock_polarity_high(void) { SPCR |= _BV(CPOL); }
-static inline void
-spi_clock_polarity_low(void)  { SPCR &= ~(_BV(CPOL)); }
-static inline void
-spi_clock_phase_first(void)   { SPCR |= _BV(CPHA); }
-static inline void
-spi_clock_phase_last(void)    { SPCR &= ~(_BV(CPHA)); }
-
-static inline void
-spi_clock_d4(void)
-{
-	SPCR = SPCR & ~(_BV(SPR1) | _BV(SPR0));
-}
-
-static inline void
-spi_clock_d16(void)
-{
-	SPCR = (SPCR & ~(_BV(SPR1))) | _BV(SPR0);
-}
-
-static inline void
-spi_clock_d64(void)
-{
-	SPCR = (SPCR & ~(_BV(SPR0))) | _BV(SPR1);
-}
-
-static inline void
-spi_clock_d128(void)
-{
-	SPCR = SPCR | _BV(SPR1) | _BV(SPR1);
-}
 
 #define spi_interrupt() ISR(SPI_STC_vect)
 #define spi_interrupt_naked() ISR(SPI_STC_vect, ISR_NAKED)
@@ -79,15 +74,13 @@ spi_clock_d128(void)
 
 static inline uint8_t
 spi_interrupt_flag(void)       { return SPSR & _BV(SPIF); }
-static inline void
-spi_interrupt_flag_clear(void) { SPSR |= _BV(SPIF); }
 static inline uint8_t
 spi_write_collision(void)      { return SPSR & _BV(WCOL); }
 
 static inline void
-spi_clock_t2(void)             { SPSR |= _BV(SPI2X); }
+spi_double_speed_disable(void) { SPSR = 0; }
 static inline void
-spi_clock_t1(void)             { SPSR &= ~(_BV(SPI2X)); }
+spi_double_speed_enable(void)  { SPSR = _BV(SPI2X); }
 
 static inline uint8_t
 spi_read(void)                 { return SPDR; }
